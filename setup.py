7134ed8c8732fe9314b316fd272754dfdf1d45c5
Zethson <lukas.heumos@gmail.com>
2019-04-09 10:43:14 +0200

    [FEATURE] first draft of setuppy

--- setup.py
+++ setup.py
@@ -6,22 +6,17 @@ from distutils.core import setup
 requirements = [line.strip() for line in open('requirements.txt')]
 
 setup(
-    name='guideseq',
-    version='0.9.0',
+    name='guideseq_wf',
+    version='1.0.0',
     description="An easy to use bioinformatic pipeline for the GUIDE-seq assay.",
-    author="Shengdar Q Tsai, Martin Aryee, Ved V Topkar",
-    author_email='STSAI4@mgh.harvard.edu, Aryee.Martin@mgh.harvard.edu, vedtopkar@gmail.com',
-    url='https://github.com/vedtopkar/guideseq',
-    packages=[
-        'guideseq',
-    ],
-    package_dir={'guideseq':
-                 'guideseq'},
+    author="based on work from Shengdar Q Tsai, Martin Aryee, Ved V Topkar, extended by Lukas Heumos",
+    author_email='STSAI4@mgh.harvard.edu, Aryee.Martin@mgh.harvard.edu, vedtopkar@gmail.com, lukas.heumos@posteo.net',
+    url='https://github.com/Zethson/guide_seq_wf',
+    scripts=['scripts/guideseq'],
     install_requires=requirements,
     license="AGPL",
     keywords='guideseq',
     classifiers=[
-        'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Scientific/Engineering :: Visualization',
