72b9e5af8dfd11ca53cde40cc5ab78a25cbf97a9
Zethson <lukas.heumos@gmail.com>
2019-04-09 10:48:03 +0200

    [FIX] using setuptools instead of distutils setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools.setup import setup
 
 requirements = [line.strip() for line in open('requirements.txt')]
 
