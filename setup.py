7a5481a027cddf1582676c94cb5bdf113a70085a
Zethson <lukas.heumos@gmail.com>
2019-04-09 12:54:04 +0200

    [FIX] hopefully scripts are all copied now

--- setup.py
+++ setup.py
@@ -3,30 +3,32 @@
 
 from setuptools import setup
 
-requirements = [line.strip() for line in open('requirements.txt')]
+version='1.0.0'
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name='guideseq_wf',
-    version='1.0.0',
+    version=version,
     description="An easy to use bioinformatic pipeline for the GUIDE-seq assay.",
+    long_description=readme,
     author="based on work from Shengdar Q Tsai, Martin Aryee, Ved V Topkar, extended by Lukas Heumos",
     author_email='STSAI4@mgh.harvard.edu, Aryee.Martin@mgh.harvard.edu, vedtopkar@gmail.com, lukas.heumos@posteo.net',
     url='https://github.com/Zethson/guide_seq_wf',
-    include_package_data=True,
     scripts=['scripts/guideseq'],
-    install_requires=requirements,
-    license="AGPL",
-    keywords='guideseq',
-    classifiers=[
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Topic :: Scientific/Engineering :: Visualization',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-        'Operating System :: Unix',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7'
-    ]
+    install_requires=required,
+    include_package_data=True,
+    packages=find_packages(),
+    license=license,
+    keywords='guideseq'
 )
+
+
+
