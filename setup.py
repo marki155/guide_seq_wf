cb4ea8075212c6b3e0371bf30f1b093a375eea1c
Zethson <lukas.heumos@gmail.com>
2019-04-09 14:20:42 +0200

    [FEATURE] renamed to g-seq

--- setup.py
+++ setup.py
@@ -15,14 +15,14 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='guideseq_wf',
+    name='g-seq',
     version=version,
-    description="An easy to use bioinformatic pipeline for the GUIDE-seq assay.",
+    description="An easy to use bioinformatics pipeline for the GUIDE-seq assay.",
     long_description=readme,
     author="based on work from Shengdar Q Tsai, Martin Aryee, Ved V Topkar, extended by Lukas Heumos",
     author_email='STSAI4@mgh.harvard.edu, Aryee.Martin@mgh.harvard.edu, vedtopkar@gmail.com, lukas.heumos@posteo.net',
     url='https://github.com/Zethson/guide_seq_wf',
-    scripts=['scripts/guideseq'],
+    scripts=['scripts/g-seq'],
     install_requires=required,
     include_package_data=True,
     packages=find_packages(),
